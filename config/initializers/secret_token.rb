07c62d7b30cc32b12f149332499c8610f7a0ad75
Francisco Mendoza <francisco.mendoza@envato.com>
2018-06-07 17:01:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeadingsProto::Application.config.secret_token = '9aa0994f23ed8a3a11dc2f7d1aadac50e03b1d5334268605d9c5cc684839c4d38b20eb11d6333b7f1bdf64d3650487182f9c66e402ffc355a213db9c72e5f127'
