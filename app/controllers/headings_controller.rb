20bc215a2c38d1cb052fc046f968aa8de6c29587
Francisco Mendoza <francisco.mendoza@envato.com>
2018-06-08 16:00:09 -0500

    Add changes

--- app/controllers/headings_controller.rb
+++ app/controllers/headings_controller.rb
@@ -8,6 +8,7 @@ class HeadingsController < ApplicationController
     end
 
     def new
+        @heading = Heading.new
     end
 
     def edit
@@ -21,12 +22,45 @@ class HeadingsController < ApplicationController
             u.heading = params[:heading]
             u.subtitle = params[:subtitle]
             u.solid_color = params[:solid_color]
+
+            # slab1
+            u.slab1_color = params[:slab1_color]
+            u.slab1_opacity = params[:slab1_opacity]
+            u.slab1_txt_color = params[:slab1_txt_color]
+            u.slab1_txt_opacity = params[:slab1_txt_opacity]
+            u.slab1_txt_roll_color = params[:slab1_txt_roll_color]
+            u.slab1_txt_roll_opacity = params[:slab1_txt_roll_opacity]
+            u.slab1_txt_sel_color = params[:slab1_txt_sel_color]
+            u.slab1_txt_sel_opacity = params[:slab1_txt_sel_opacity]
         end
         @heading.save
         redirect_to headings_path
     end
 
     def update
+        @heading = Heading.find(params[:id])
+
+        @heading.tap do |u|
+            u.meta_title = params[:meta_title]
+            u.meta_description = params[:meta_description]
+            u.heading = params[:heading]
+            u.subtitle = params[:subtitle]
+            u.solid_color = params[:solid_color]
+
+            # slab1
+            u.slab1_color = params[:slab1_color]
+            u.slab1_opacity = params[:slab1_opacity]
+            u.slab1_txt_color = params[:slab1_txt_color]
+            u.slab1_txt_opacity = params[:slab1_txt_opacity]
+            u.slab1_txt_roll_color = params[:slab1_txt_roll_color]
+            u.slab1_txt_roll_opacity = params[:slab1_txt_roll_opacity]
+            u.slab1_txt_sel_color = params[:slab1_txt_sel_color]
+            u.slab1_txt_sel_opacity = params[:slab1_txt_sel_opacity]
+        end
+
+        if !@heading.save
+            render 'edit'
+        end
         redirect_to headings_path
     end
 
